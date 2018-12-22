7e21f1e914493886108d812e3363714ae469e85f
Bradley Treweek <cmd430@gmail.com>
2018-12-22 22:43:05 +1300

    Inital Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Example Usage: 
+  run `node index`
+  open your browser to `http://localhost:5000/https://www.google.com/`
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')()
+const request = require('request')
+const config = {
+  port: 5000
+}
+express.all('*', (req, res, next) => {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': res.get('access-control-request-headers') || 'Authorization'
+  })
+  if (req.method === 'OPTIONS') { // CORS Preflight
+    return res.send()
+  } else {
+    let targetURL = req.url.slice(1)
+    if (!targetURL) {
+      return res.status(400).json({ 
+        error: 'missing url' 
+      })
+    }
+    request({ 
+      url: targetURL, 
+      method: req.method, 
+      json: req.body, 
+      headers: {
+        'Authorization': req.header('Authorization') || 'noauth'
+      } 
+    }, (err, response, body) => {}).pipe(res)
+  }
+})
+express.listen(config.port, () => {
+  console.log(`CORS Proxy listening on port ${config.port}`)
+})

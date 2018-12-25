26e0fef56461c9428184b9fe90403f8b15123e70
Bradley Treweek <cmd430@gmail.com>
2018-12-25 18:16:17 +1300

    allow setting headers sent to requested url

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ const config = {
       // allow any request with a whitelisted
       // origin to use the CORS proxy.
       //
-      // example: https://example.com
+      // example: 'https://example.com'
+    ],
+    passthrough: [
+      // Any headers you want to allow
+      // too be passed through to the
+      // proxied request
+      //
+      // example: 'Authorization'
     ]
   },
   cors: {
@@ -23,20 +30,30 @@ const config = {
   }
 }
 
+function passthrough (headers) {
+  let result = {}
+  config.app.passthrough.forEach(passthrough => {
+    let header = headers[passthrough.toLowerCase()]
+    if (header) {
+      result[passthrough] = header
+    }
+  })
+  return result
+}
+
 app.route('*')
 .options(cors(config.cors))
 .all(cors(config.cors), (req, res, next) => {
+  let targetURL = req.url.slice(1)
   if (config.app.restricted && !config.app.whitelist.includes(req.headers['origin'])) {
     return res.status(401).json({ error: 'unauthorized' })
-  }
-  let targetURL = req.url.slice(1)
-  if (!targetURL || !targetURL.startsWith('http')) {
+  } else if (!targetURL || !targetURL.startsWith('http')) {
     return res.status(400).json({ error: 'missing valid url' })
   }
   request({
     url: targetURL,
     method: req.method,
-    headers: req.headers
+    headers: passthrough(req.headers)
   })
   .pipe(res)
 })

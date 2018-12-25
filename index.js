794f034101b3c25942b4ae1ba137f790d9cdd586
Bradley Treweek <cmd430@gmail.com>
2018-12-25 17:21:11 +1300

    allow whitelist for CORS proxy usage

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const request = require('request')
 const app = express()
+
 const config = {
   app: {
-    port: 80
+    port: 80,
+    restricted: false,
+    whitelist: [
+      // if restricted is enabled will only
+      // allow any request with a whitelisted
+      // origin to use the CORS proxy.
+      //
+      // example: https://example.com
+    ]
   },
   cors: {
     origin: '*',
@@ -17,9 +26,12 @@ const config = {
 app.route('*')
 .options(cors(config.cors))
 .all(cors(config.cors), (req, res, next) => {
+  if (config.app.restricted && !config.app.whitelist.includes(req.headers['origin'])) {
+    return res.status(401).json({ error: 'unauthorized' })
+  }
   let targetURL = req.url.slice(1)
-  if (!targetURL) {
-    return res.status(400).json({ error: 'missing url' })
+  if (!targetURL || !targetURL.startsWith('http')) {
+    return res.status(400).json({ error: 'missing valid url' })
   }
   request({
     url: targetURL,
@@ -32,5 +44,5 @@ app.route('*')
 })
 
 app.listen(config.app.port, () => {
-  console.log(`CORS proxy started`)
+  console.log('CORS proxy started')
 })
